10ceecfea5e4d387bb4ac7eafab4d73441d97622
edss1992 <a403481704@163.com>
2016-09-29 10:58:11 +0800

    test.py

---
+++ com/ehealth/test/test.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 29, 2016
+
+@author: johnw
+'''
+
+print "Here is test again!"
+print "I will keep my work"
+print "Stay hungry stay foolish!"
+print "test and develop"
+print "what is pythoner?"
+print "life is short and pain so i use python"
+print "I just don't know how to say that.."
+print "It is ok to commit!"
